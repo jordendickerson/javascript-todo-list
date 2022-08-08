c53777995972d92c0f46f50bcf83ce1a2c8d70c6
jordendickerson <jordendickerson24@gmail.com>
2022-08-08 15:50:34 -0600

    Added basis for project adding

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 require('./style/main.scss');
+
+const todoContainer = document.getElementById('todos-container');
+const addProjectButton = document.querySelector('.add-project-button');
+
+function projectSidebarHandler(){
+    const projectsSidebar = document.querySelector('.projects-wrapper');
+    const projectsList = [];
+
+    const addItem = (itemName) => {
+        let item = itemName;
+        projectsList.push(item);
+        projectsSidebar.innerHTML = '';
+
+        updateProjects();
+    }
+
+
+    const updateProjects = () => projectsList.map((item) => {
+        const projectsSidebarItem = document.createElement('li');
+        projectsSidebarItem.textContent = item;
+        projectsSidebarItem.classList.add('project-sidebar-item');
+
+        projectsSidebar.appendChild(projectsSidebarItem);
+    });
+
+    return {addItem, updateProjects};
+}
+
+const projectSidebar = projectSidebarHandler();
+projectSidebar.updateProjects();
+let projectNum = 1;
+addProjectButton.addEventListener("click", () => {
+    projectSidebar.addItem("Project "+projectNum);
+    projectNum++;
+})
