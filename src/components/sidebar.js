6648197f2d6aebda7c61dd6730769b20312309a8
jordendickerson <jordendickerson24@gmail.com>
2022-08-12 00:46:13 -0600

    Added ability to create named projects

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,14 @@
 function projectSidebarHandler(){
     const projectsSidebar = document.querySelector('.projects-wrapper');
+    const addProjectForm = document.querySelector('.add-project-form');
+    const projectTitleInput = document.querySelector('.project-title-input');
+    const submitProjectButton = document.querySelector('.submit-project-button');
+    const cancelButton = document.querySelector('.cancel-button');
+    const listTitle = document.querySelector('.list-title');
+
     const projectsList = [];
 
+    
     const addItem = (itemName) => {
         let item = itemName;
         projectsList.push(item);
@@ -12,14 +19,39 @@ function projectSidebarHandler(){
 
 
     const updateProjects = () => projectsList.map((item) => {
-        const projectsSidebarItem = document.createElement('li');
+        const projectsSidebarItem = document.createElement('div');
         projectsSidebarItem.textContent = item;
         projectsSidebarItem.classList.add('project-sidebar-item');
+        projectsSidebarItem.addEventListener("click", () => {
+            listTitle.textContent = item + " Tasks";
+        });
 
         projectsSidebar.appendChild(projectsSidebarItem);
     });
 
-    return {addItem};
+    const showProjectForm = () => {
+        addProjectForm.classList.toggle('inactive');
+    }
+
+    submitProjectButton.addEventListener("click", () => {
+        if (projectTitleInput.value){
+            addItem(projectTitleInput.value);
+        }
+        projectTitleInput.value = '';
+        addProjectForm.classList.toggle('inactive');
+    })
+
+    cancelButton.addEventListener("click", () => {
+        addProjectForm.classList.toggle('inactive');
+        projectTitleInput.value = '';
+    })
+
+
+    
+
+    return {showProjectForm};
 }
+
+
 
 export default projectSidebarHandler;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('./style/main.scss');
 import projectSidebarHandler from './components/sidebar';
 
-const listTitle = document.querySelector('.list-title');
 const tasksContainer = document.querySelector('.tasks-container');
 const addTaskButton = document.querySelector('.add-task-button')
 const addProjectButton = document.querySelector('.add-project-button');
@@ -12,13 +11,14 @@ function app(){
     let filter = 'all';
     let tasks = [];
 
+    
+
     //Sidebar
     const projectSidebar = projectSidebarHandler();
-    let projectNum = 1;
     addProjectButton.addEventListener("click", () => {
-        projectSidebar.addItem("Project "+projectNum);
-        projectNum++;
+        projectSidebar.showProjectForm();
     })
+    
 
     //Add Task Button
     addTaskButton.addEventListener("click", () => {
